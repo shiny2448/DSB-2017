fb0e060bacaee1826f8c7c5b8cad1550d2abe604
shiny2448 <shiny2448@gmail.com>
2017-03-06 23:35:52 -0800

    Low accuracy demo with a single slice
    Unzipping the dataset takes 4 hours, so if there is anyone who already have unzipped file, please try to run it. I'll try it later as well.

---
+++ project.py
@@ -0,0 +1,99 @@
+import dicom
+import os.path
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+#MAIN_FOLDER = "sample_images"
+MAIN_FOLDER = "stage1/stage1"
+SCALE_CONST = 16
+patient_id_list = [name for name in os.listdir(MAIN_FOLDER)]
+
+def compute_score(labels,pred):
+	error_count = 0
+	for tup in zip(pred,labels):
+		if tup[0]!=tup[1]:
+			error_count+=1
+	return float(labels.shape[0]-error_count)/float(labels.shape[0])
+
+def LDA_predict(cov,x_list,pi_list,mean_list):
+    inv_cov = np.linalg.pinv(cov+hyper_param*np.identity(cov.shape[0]))
+    log_det = sum([math.log(eig+hyper_param) for eig in np.linalg.eigvals(cov)])
+    pred = []
+    for x in x_list:
+        estimated_g = []
+        for i in range(len(mean_list)):
+            estimated_g.append(-0.5*np.dot(np.dot((x-mean_list[i]).T,inv_cov),(x-mean_list[i]))+math.log(pi_list[i])-0.5*log_det)
+            #estimated_g.append(np.dot(np.dot(x.T,inv_cov),mean_list[i])-0.5*np.dot(np.dot(mean_list[i].T,inv_cov),mean_list[i])+math.log(pi_list[i]))
+        pred.append(np.argmax(estimated_g))
+    return pred
+
+def QDA_predict(cov_list,x_list,pi_list,mean_list):
+    pred = []
+    inv_cov_list = [np.linalg.pinv(cov_list[i]+hyper_param*np.identity(cov_list[i].shape[0])) for i in range(len(mean_list))]
+    eigvals = np.linalg.eigvals(cov_list[i])
+    log_det_list = [sum([math.log(eig+hyper_param) for eig in eigvals]) for i in range(len(mean_list))]
+    
+    for x in x_list:
+        estimated_g = []
+        for i in range(len(mean_list)):
+            inv_cov = inv_cov_list[i]
+            estimated_g.append(-0.5*np.dot(np.dot((x-mean_list[i]).T,inv_cov),(x-mean_list[i]))+math.log(pi_list[i])-0.5*(log_det_list[i]))
+        pred.append(np.argmax(estimated_g))
+    return pred
+#print patient_id_list
+#df_labels = pd.read_csv("stage1_labels.csv")
+#df_labels = df_labels.drop('index')
+#print df_labels[(df_labels['id']=='0015ceb851d7251b8f399e39779d1e7d')].loc[:,'cancer']
+label_list = {}
+with open("stage1_labels.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        if row[1]=='0' or row[1]=='1':
+            label_list[row[0]]=row[1]
+            
+test_set = []
+with open("stage1_sample_submission.csv") as csvfile:
+    reader = csv.reader(csvfile,delimiter=',')
+    for row in reader:
+        if row[0]!='id':
+            test_set.append(row[0])
+
+validation_set = np.array(mid_images[:len(mid_images)//5])
+training_set = np.array(mid_images[len(mid_images)//5:])
+nums_samples = [len(mid_images)]
+#nums_samples = [100,200,500,1000,len(mid_images)]
+for num_samples in nums_samples:
+    v_set = validation_set[:,:validation_set.shape[1]-1]
+    v_label = validation_set[:,validation_set.shape[1]-1]
+    t_set = training_set[:num_samples,:training_set.shape[1]-1]
+    t_label = training_set[:num_samples,training_set.shape[1]-1]
+    classes = [[] for _ in range(num_classes)]
+    classes_means = []
+    classes_covs = []
+    for i in range(len(t_label)):
+        classes[int(t_label[i])].append(t_set[i])
+    for class_label in classes:
+        class_label = np.array(class_label)
+        classes_means.append(np.mean(class_label,axis=0))
+        classes_covs.append(np.cov(class_label.T))
+    mean_cov = np.array([[0 for _ in range(t_set.shape[1])] for _ in range(t_set.shape[1])])
+    for cov in classes_covs:
+        mean_cov = mean_cov+cov
+    mean_cov = mean_cov/float(num_classes)
+    pi_list = [float(len(class_label))/float(t_set.shape[0]) for class_label in classes]
+    LDA_pred = LDA_predict(mean_cov,v_set,pi_list,classes_means)
+    QDA_pred = QDA_predict(classes_covs,v_set,pi_list,classes_means)
+    LDA_score = compute_score(v_label,LDA_pred)
+    QDA_score = compute_score(v_label,QDA_pred)
+    LDA_errors.append(1.0-LDA_score)
+    QDA_errors.append(1.0-QDA_score)
+plt.plot(nums_samples,LDA_errors,'r-',label='LDA error')
+plt.plot(nums_samples,QDA_errors,'b-',label='QDA error')
+plt.title('Num Samples vs. Error Rate')
+plt.xlabel('Number of Samples')
+plt.ylabel('Error Rate')
+plt.show()
+print "LDA errors:",LDA_errors
+print "QDA_errors:",QDA_errors
